ff31a074b0361f105d8192ca9c39356fb7e5aaba
lffmribeiro <lffmribeiro@sabesp.com.br>
2024-01-15 13:49:28 -0300

    Acréscimo do agendamento

--- Main.py
+++ Main.py
@@ -1,7 +1,18 @@
 import ExtracaoBancoOracle as ex
 
+#Agendamento
+import schedule
+import time
+
+#Instanciando a classe que fará a extracao
 ext = ex.ExtracaoBancoOracle(caminhoSQL_TXT='C:\\Users\\id_teste\\Downloads', 
                              nomeTXT_SQL='teste.sql', 
                              caminhoSalvarArquivo='C:\\Users\\id_teste\\Downloads', 
                              nomeArquivo='testeExtracao.csv')
-ext.extrairSalvarArquivo()
+
+#Agendamento
+schedule.every.day.at('00:00').do(ext.extrairSalvarArquivo())
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
